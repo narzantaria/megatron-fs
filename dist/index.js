83e31fad073d2b806070af763d5b18f13d3ebe91
Nar Zantaria <narzantaria@gmail.com>
2024-06-07 16:21:37 +0300

    version-1.0.6

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.rwi = exports.iwi = exports.replaceWithIndent = exports.insertWithIndent = exports.addIndents = exports.readSchema = void 0;
-const promises_1 = require("./src/promises");
-const UNIVERSAL_FOLDER = `${process.cwd()}/universal`;
-// Read universal schema
-async function readSchema(arg) {
-    try {
-        const rawData = await (0, promises_1.read)(`${UNIVERSAL_FOLDER}/${arg}.json`, "utf8");
-        if (!rawData)
-            return null;
-        const data = JSON.parse(rawData);
-        return data;
-    }
-    catch (error) {
-        console.log(error);
-        return null;
-    }
-}
-exports.readSchema = readSchema;
+exports.rwi = exports.iwi = exports.smartClean = exports.smartCopy = exports.stringBetweenStrings = exports.replaceWithIndent = exports.insertWithIndent = exports.addIndents = void 0;
+const path_1 = __importDefault(require("path"));
+const promiseman_1 = require("promiseman");
 function repeatStr(str, length) {
     return length > 0 && str.length
         ? Array.from({ length })
@@ -57,3 +45,98 @@ function replaceWithIndent(data, code, marker) {
 }
 exports.replaceWithIndent = replaceWithIndent;
 exports.rwi = replaceWithIndent;
+function stringBetweenStrings(startStr, endStr, str) {
+    let pos = str.indexOf(startStr) + startStr.length;
+    return str.substring(pos, str.indexOf(endStr, pos));
+}
+exports.stringBetweenStrings = stringBetweenStrings;
+/**
+ * Копирует исходники из папки template/main в папку проекта (release),
+ * однако не трогает определенные файлы и папки
+ **/
+async function smartCopy(sourceDir, targetDir, ignoreList) {
+    const files = await (0, promiseman_1.readdir)(sourceDir);
+    for (const file of files) {
+        if (ignoreList.includes(file)) {
+            continue;
+        }
+        const filePath = `${sourceDir}/${file}`;
+        const targetPath = `${targetDir}/${file}`;
+        const stats = await (0, promiseman_1.stat)(filePath);
+        if (stats.isDirectory()) {
+            await (0, promiseman_1.mkdir)(targetPath, { recursive: true });
+            await smartCopy(filePath, targetPath, ignoreList);
+        }
+        else {
+            await (0, promiseman_1.copyFile)(filePath, targetPath);
+        }
+    }
+}
+exports.smartCopy = smartCopy;
+// Удаление папки и всего содержимого рекурсивно?
+async function deleteFolderRecursive(folderPath) {
+    let files = [];
+    try {
+        files = await (0, promiseman_1.readdir)(folderPath);
+    }
+    catch (err) {
+        // Если папка не существует, просто выходим из функции
+        return;
+    }
+    for (const file of files) {
+        const curPath = path_1.default.join(folderPath, file);
+        try {
+            const stats = await (0, promiseman_1.lstat)(curPath);
+            if (stats.isDirectory()) {
+                // Рекурсивный вызов для удаления поддиректорий
+                await deleteFolderRecursive(curPath);
+            }
+            else {
+                // Удаление файла
+                await (0, promiseman_1.unlink)(curPath);
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+    // Удаление самой папки
+    try {
+        await (0, promiseman_1.rmdir)(folderPath);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+/**
+ * Чистит папку однако не трогает определенные файлы и папки
+ * и не трогает саму папку
+ **/
+async function smartClean(dir, ignore) {
+    // const releaseFolderPath = path.resolve(pathToRelease);
+    try {
+        // Читаем содержимое папки "release"
+        const files = await (0, promiseman_1.readdir)(dir);
+        // Проходимся по каждому файлу/папке
+        for (const file of files) {
+            const filePath = path_1.default.join(dir, file);
+            // Проверяем, является ли текущий файл/папка папкой "data" или файлом ".env"
+            if (!ignore?.some((x) => x === file)) {
+                // Получаем информацию о текущем файле/папке
+                const stats = await (0, promiseman_1.lstat)(filePath);
+                if (stats.isFile()) {
+                    // Если это файл, то удаляем его
+                    await (0, promiseman_1.unlink)(filePath);
+                }
+                else if (stats.isDirectory()) {
+                    // Если это папка, то удаляем ее и все ее содержимое рекурсивно
+                    await deleteFolderRecursive(filePath);
+                }
+            }
+        }
+    }
+    catch (error) {
+        console.error(`Failed to delete content in ${dir}: ${error}`);
+    }
+}
+exports.smartClean = smartClean;
--- index.ts
+++ index.ts
@@ -1,19 +1,62 @@
-import { IField } from "./src/types";
-import { read } from "./src/promises";
+import path from "path";
+import { copyFile, lstat, mkdir, read, readdir, rmdir, stat, unlink } from "promiseman";
 
-const UNIVERSAL_FOLDER = `${process.cwd()}/universal`;
+export type TType =
+  | "int"
+  | "double"
+  | "string"
+  | "description"
+  | "text"
+  | "code"
+  | "json"
+  | "password"
+  | "phone"
+  | "phonex"
+  | "email"
+  | "select"
+  | "radio"
+  | "img"
+  | "gallery"
+  | "video"
+  | "checkbox"
+  | "tags"
+  | "color"
+  | "switch"
+  | "date"
+  | "vendor"
+  | "complex"
+  | "multiselect"
+  | "relation";
 
-// Read universal schema
-export async function readSchema(arg: string): Promise<IField[] | null> {
-  try {
-    const rawData = await read(`${UNIVERSAL_FOLDER}/${arg}.json`, "utf8");
-    if (!rawData) return null;
-    const data: IField[] = JSON.parse(rawData);
-    return data;
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
+export interface IObject {
+  [key: string]: any;
+}
+
+export interface StepLevel {
+  name: string;
+  label: string;
+}
+
+export interface IField extends IObject {
+  name: string;
+  label?: string;
+  type: TType;
+  required?: boolean;
+  schema?: "one-to-one" | "one-to-many" | "many-to-many" | "many-to-many-bi";
+  level?: "host" | "recipient" | "side";
+  ref?: string;
+  refname?: string;
+  unique?: boolean;
+  array?: boolean;
+  nocascade?: boolean;
+  extra?: any;
+  default?: any;
+  steps?: StepLevel[];
+}
+
+export interface Model {
+  name: string;
+  data: string;
 }
 
 function repeatStr(str: string, length: number): string {
@@ -65,6 +108,111 @@ export function replaceWithIndent(
   const codeWithIndents = addIndents(code, indent);
   const result = data.replace(marker, codeWithIndents);
   return result;
+}
+
+export function stringBetweenStrings(
+  startStr: string,
+  endStr: string,
+  str: string
+): string {
+  let pos = str.indexOf(startStr) + startStr.length;
+  return str.substring(pos, str.indexOf(endStr, pos));
+}
+
+/**
+ * Копирует исходники из папки template/main в папку проекта (release),
+ * однако не трогает определенные файлы и папки
+ **/
+export async function smartCopy(sourceDir: string, targetDir: string, ignoreList: string[]) {
+  const files = await readdir(sourceDir);
+
+  for (const file of files) {
+    if (ignoreList.includes(file)) {
+      continue;
+    }
+
+    const filePath = `${sourceDir}/${file}`;
+    const targetPath = `${targetDir}/${file}`;
+
+    const stats = await stat(filePath);
+
+    if (stats.isDirectory()) {
+      await mkdir(targetPath, { recursive: true });
+      await smartCopy(filePath, targetPath, ignoreList);
+    } else {
+      await copyFile(filePath, targetPath);
+    }
+  }
+}
+
+// Удаление папки и всего содержимого рекурсивно?
+async function deleteFolderRecursive(folderPath: string) {
+  let files = [];
+
+  try {
+    files = await readdir(folderPath);
+  } catch (err) {
+    // Если папка не существует, просто выходим из функции
+    return;
+  }
+
+  for (const file of files) {
+    const curPath = path.join(folderPath, file);
+
+    try {
+      const stats = await lstat(curPath);
+      if (stats.isDirectory()) {
+        // Рекурсивный вызов для удаления поддиректорий
+        await deleteFolderRecursive(curPath);
+      } else {
+        // Удаление файла
+        await unlink(curPath);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // Удаление самой папки
+  try {
+    await rmdir(folderPath);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * Чистит папку однако не трогает определенные файлы и папки
+ * и не трогает саму папку
+ **/
+export async function smartClean(dir: string, ignore?: string[]) {
+  // const releaseFolderPath = path.resolve(pathToRelease);
+
+  try {
+    // Читаем содержимое папки "release"
+    const files = await readdir(dir);
+
+    // Проходимся по каждому файлу/папке
+    for (const file of files) {
+      const filePath = path.join(dir, file);
+
+      // Проверяем, является ли текущий файл/папка папкой "data" или файлом ".env"
+      if (!ignore?.some((x) => x === file)) {
+        // Получаем информацию о текущем файле/папке
+        const stats = await lstat(filePath);
+
+        if (stats.isFile()) {
+          // Если это файл, то удаляем его
+          await unlink(filePath);
+        } else if (stats.isDirectory()) {
+          // Если это папка, то удаляем ее и все ее содержимое рекурсивно
+          await deleteFolderRecursive(filePath);
+        }
+      }
+    }
+  } catch (error) {
+    console.error(`Failed to delete content in ${dir}: ${error}`);
+  }
 }
 
 export { insertWithIndent as iwi, replaceWithIndent as rwi }
